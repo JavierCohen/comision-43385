6985142dc5b378c88c267e8347955421fdb66328
Javier Cohen <javiercohenn@gmail.com>
2023-06-30 08:59:02 -0300

    2daPreentrega-2

--- ProductManager.js
+++ ProductManager.js
@@ -1,39 +1,55 @@
+import fs from 'fs';
+
 class ProductManager {
-    constructor() {
+    constructor(path) {
+        this.path = path;
         this.products = [];
-        this.productIdCounter = 1; // Contador para generar IDs autoincrementables
+        this.productIdCounter = 1;  // Contador para generar IDs autoincrementables
+        this.loadProducts();    // Cargar productos desde el archivo al iniciar
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
-    // Validar que todos los campos sean obligatorios
+    loadProducts() {
+        try {
+            const data = fs.readFileSync(this.path, 'utf8');
+            this.products = JSON.parse(data);
+        } catch (error) {
+            // Si ocurre un error al leer el archivo, se asume que no existe o está vacío
+            this.products = [];
+        }
+    }
+
+    saveProducts() {
+        const data = JSON.stringify(this.products, null, 2);
+        fs.writeFileSync(this.path, data, 'utf8');
+    }
+
+    addProduct(product) {
+        // Validar que todos los campos sean obligatorios
+        const { title, description, price, thumbnail, code, stock } = product;
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             console.log("Error: Todos los campos son obligatorios");
             return;
         }
 
         // Validar que no se repita el campo "code"
-        const existingProduct = this.products.find(
-            (product) => product.code === code
-        );
+        const existingProduct = this.products.find((p) => p.code === code);
         if (existingProduct) {
             console.log("Error: Ya existe un producto con el mismo código");
             return;
         }
 
-        // Agregar el producto al arreglo de productos
+        // Asignar un ID autoincrementable al producto
         const newProduct = {
             id: this.productIdCounter,
-            title: title,
-            description: description,
-            price: price,
-            thumbnail: thumbnail,
-            code: code,
-            stock: stock,
+            ...product,
         };
         this.products.push(newProduct);
 
         // Incrementar el contador de IDs
         this.productIdCounter++;
+
+        // Guardar los productos en el archivo
+        this.saveProducts();
 
         console.log("Producto agregado correctamente");
     }
@@ -43,36 +59,37 @@ class ProductManager {
     }
 
     getProductById(id) {
-        const product = this.products.find((product) => product.id === id);
+        const product = this.products.find((p) => p.id === id);
         if (product) {
             return product;
+        } else {
+            throw new Error("Error: Producto no encontrado");
+        }
+    }
+
+    updateProduct(id, updatedFields) {
+        const productIndex = this.products.findIndex((p) => p.id === id);
+        if (productIndex !== -1) {
+            // Mantener el ID original del producto
+            updatedFields.id = id;
+            this.products[productIndex] = { ...this.products[productIndex], ...updatedFields };
+            this.saveProducts();
+            console.log("Producto actualizado correctamente");
+        } else {
+            console.log("Error: Producto no encontrado");
+        }
+    }
+
+    deleteProduct(id) {
+        const productIndex = this.products.findIndex((p) => p.id === id);
+        if (productIndex !== -1) {
+            this.products.splice(productIndex, 1);
+            this.saveProducts();
+            console.log("Producto eliminado correctamente");
         } else {
             console.log("Error: Producto no encontrado");
         }
     }
 }
 
-// Ejemplo de uso
-const productManager = new ProductManager();
-productManager.addProduct(
-    "Producto 1",
-    "Descripción 1",
-    10,
-    "thumbnail1.jpg",
-    "code1",
-    5
-);
-productManager.addProduct(
-    "Producto 2",
-    "Descripción 2",
-    20,
-    "thumbnail2.jpg",
-    "code2",
-    3
-);
-
-const allProducts = productManager.getProducts();
-console.log(allProducts);
-
-const productById = productManager.getProductById(2);
-console.log(productById);
+export default ProductManager;
